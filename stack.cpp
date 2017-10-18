66f7a1c976da12137b74ca843446a905180c89da
Bhanu Pratap Raghav <bhanuraghav1999@gmail.com>
2017-10-19 01:26:43 +0530

    Stack code added

--- stack.cpp
+++ stack.cpp
@@ -1,57 +1,61 @@
+//Stack_Code
+// Complexity: O(1)
+
 #include <iostream.h>
-#include<process.h>
+#include <process.h>
 #include <conio.h>
 struct node
-{
-	int number;
-	node *next;
+{												//declaring a structure	
+	int number;									
+	node *next;									//self referential pointer
 };
-node *push(node *top, int val);
-node *pop(node *top);
+node *push(node *top, int val);					//fuction to push element into stack			
+node *pop(node *top);							//fuction to pop element from stack
 void show_Stack(node *top);
-void main()
-{       clrscr();
-	node *top;
+int main()
+{       
+	node *top;									
 	int tnumber, choice;
 	char ch;
 	top = NULL;
 	clrscr();
-	do
+	do											//menu showing choices to the user
 	{	cout << "\n\t Main Menu";
 		cout << "\n\t1. ADD TO Stack";
 		cout << "\n\t2. DELETE from Stack";
 		cout << "\n\t3. SHOW Stack";
 		cout << "\n\t4. Exit from Menu";
 		cout << "\n\nEnter your choice :  ";
-		cin >> choice;
+		cin >> choice;							//input of choice from user
 		switch (choice)
 		{       case 1: do
 						{   	cout << "Enter the no. : ";
 								cin >> tnumber;
-								top = push(top, tnumber);
+								top = push(top, tnumber);		//call the push() function
 								cout << "Do you want to add more elements <Y/N> ?:  ";
 								cin >>ch;
 						} while (ch== 'Y'||ch=='y');
 				break;
 
 				case 2: do
-						{       top = pop(top);
+						{       top = pop(top);					//call the pop() function
 								cout << "\nDo you want to delete more elements <Y/N> ?:  ";
 								cin >>ch;
 						} while (ch== 'Y'||ch=='y');
 				break;
 			
-				case 3: show_Stack(top);
+				case 3: show_Stack(top);						//call the show_Stack() function
 				break;
 			
 				case 4: exit(0);
 		}
 	}while (choice != 4);
+	return 0;
 }
 
 node *push(node *top, int val)
 {       node *temp;
-	temp = new node;
+	temp = new node;						//dynamic initialisation of element in stack
 	temp->number = val;
 	temp->next = NULL;
 	if(top ==NULL)
@@ -73,7 +77,7 @@ node *pop(node *top)
 	    top = top->next;
 	    temp->next = NULL;
 	    cout << "\n\tPopped Number is : " << temp->number;
-	    delete temp;
+	    delete temp;						//deleting the allocated memory to top element of stack
 	}
 	return (top);
 }
