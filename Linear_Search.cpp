f87d17844a902481562b776da57707e6c00b1a1c
Nikhil Seth <32011091+nikhil-seth@users.noreply.github.com>
2017-10-18 16:18:30 +0530

    Added Linear Search
    Added Linear_search.cpp File in C++

---
+++ Linear_Search.cpp
@@ -0,0 +1,34 @@
+//LINEAR SEARCH ALGORITHM
+#include<iostream>
+using namespace std;
+int linsearch(int a[],int b,int c)  //FUNCTION FOR LINEAR SEARCH TAKING ARRAY , SIZE, NO TO FIND AS ARGUMENTS
+{
+    for(int i=0;i<b;i++)         
+    {   if(a[i]==c)                 //CHECKING OF CONDITION TO FIND VARIABLE
+            return (i+1);
+    }
+    return -1;
+}
+//TIME COMPLEXITY : O(n)
+//SPACE COMPLEXITY :O(1)
+int main()
+{
+    int *arr,n,k;                   //INIT. OF ARRAY
+    cout<<"Enter no of Entries:";
+    cin>>n;                         //INPUT OF NO OF ENTRIES IN ARRAY
+    arr=new int[n];                 //DYNAMIC INITIALISATION OF ARRAY
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];                //INPUT OF ARRAY
+    }
+    cout<<"ENTER NO TO BE SEARCHED";
+    cin>>k;                         //NO TO BE SEARCHED
+    k=linsearch(arr,n,k);           //CALLING OF FUCTION LINSEARCH
+    if(k==-1)                       //  IF RETURNS -1 -> NOT FOUND ELSE PRINTING POSITION
+        cout<<"NOT FOUND";
+    else
+        cout<<"POSITON : "<<k<<endl;
+    system("pause");
+    return 0;
+}
+
