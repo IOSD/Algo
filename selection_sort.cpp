a7d55eb920d291fa47329a54da81a032196c0b82
Bhanu Pratap Raghav <bhanuraghav1999@gmail.com>
2017-10-18 23:07:31 +0530

    Added Selection Sort

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,34 +1,12 @@
+//Selection Sort
 // Time Complexity: O(n^2)
 // Space Complexity: O(1)
 
 #include<iostream>
-
 using namespace std;
 
-void selectsort(int [], int);
-
-int main()
-{	
-	int m[50],n,i;
-	cout<<"Hom many Elements , you want to enter:\n";
-	cin>>n;
-	cout<<"\nEnter the elements:\n";
-	for( i=0; i<n; i++ )
-	{	
-		cin>>m[i];
-	}
-
-	selectsort(m,n);
-	cout<<"\nSorted Array:\n";
-	for( i=0; i<n; i++)
-	{	
-		cout<<m[i]<<"\t";
-	}
-	return 0;
-}
-
-void selectsort(int p[], int size)
-{
+void selectsort(int p[], int size) 
+{										//function for selection sort, taking array and its size as arguments
 	int min,pos,temp;
 	for(int t=0; t<(size-1); t++)
 	{	min=p[t];
@@ -44,11 +22,32 @@ void selectsort(int p[], int size)
 			
 		}
 
-		temp=p[t];
+		temp=p[t];			
 		p[t]=p[pos];
 		p[pos]=temp;
 	}
 }
+
+int main()
+{	
+	int m[50],n,i;
+	cout<<"Hom many Elements , you want to enter:\n";
+	cin>>n;								//no. of elements in array user want to enter
+	cout<<"\nEnter the elements:\n";
+	for( i=0; i<n; i++ )
+	{	
+		cin>>m[i];						//taking number as input from user
+	}
+
+	selectsort(m,n);					//call selectsort function
+	cout<<"\nSorted Array:\n";
+	for( i=0; i<n; i++)							
+	{	
+		cout<<m[i]<<"\t";				//display the sorted array
+	}
+	return 0;
+}
+
 
 
 
