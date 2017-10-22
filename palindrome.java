4bb2967c71da5d42808d7306e0a9d0dba175ce0c
shankymichi <shankymichi.github.io>
2017-10-22 15:16:55 +0530

    time and space complexities added

--- palindrome.java
+++ palindrome.java
@@ -1,24 +1,27 @@
 package git;
 import java.util.*;
 
-public class pallindrome {
+// Time Complexity: O(n^3)
+// Space Complexity: O(n)
+
+public class palindrome {
 	public static void main(String args[]) {
 		int r=0,sum=0,temp;
 		Scanner input = new Scanner(System.in);
-				System.out.println("Enter the number you want to be checked for pallindrome");
-		       int n = input.nextInt();  //to take the input for "n"
-		       temp=n;
+		System.out.println("Enter the number you want to be checked for pallindrome");
+		int n = input.nextInt();  //to take the input for "n"
+		temp=n;
 		       
-		       while(n>0) {
-		    	   r=n%10;    // TO FIND REMAINDER
-		    	   sum=(sum*10)+r;
-		    	   n=n/10;      //TO REMOVE DIGIT AT ONE PLACE IN "n"
+		while(n>0) {
+			r=n%10;    // TO FIND REMAINDER
+		   	sum=(sum*10)+r;
+		   	n=n/10;      //TO REMOVE DIGIT AT ONE PLACE IN "n"
 		    	   
-		       }
-		       if(temp==sum)
-		    	   System.out.println("the number is a PALLINDROME");
-		       else
-		    	   System.out.println("the number is NOT a PALLINDROME");
+		}
+		if(temp==sum)
+			System.out.println("the number is a PALLINDROME");
+		else
+			System.out.println("the number is NOT a PALLINDROME");
 		    	   
 		
 	}
