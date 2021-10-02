de46485d3a62b9082562e3d16942fe3e914feb3d
Krishna Rai <krishna.rai30@yahoo.com>
2021-10-02 21:05:24 +0530

    Update Bubble_Sort.cpp

--- Sorting/Bubble_Sort.cpp
+++ Sorting/Bubble_Sort.cpp
@@ -2,8 +2,7 @@
 #include<iostream>
 using namespace std;
 
-void bsort(int a[],int n)       //FUNTION FOR BUBBLE SORT TAKING ARRAY AND IT'S SIZE AS ARGUMENTS
-{
+void bsort(int a[],int n)       
     int t;
     for(int i=1;i<n;i++)
         for(int j=0;j<n-i;j++)
